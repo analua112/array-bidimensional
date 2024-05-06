80acbbd24d9f55bfc92a2ffa3a55e110bfb30c02
ana <149010241+analua112@users.noreply.github.com>
2024-05-06 08:49:12 -0300

    Update arrayBidimensional.js

--- arrayBidimensional.js
+++ arrayBidimensional.js
@@ -1,6 +1,6 @@
 let nomeLanches = [ "x-buguer", "x-salada", "x-calvo", "x-cebola", "x-bacon", "x-frango", "x-tudo", "x-ratazana", "x-taco"];
 let precoLanches = [ 10.0 , 12.0 , 0.0 , 12.0 , 15.0 , 15.0 , 20.0 , 33.0, 15 ];
-let ingredientesLanches = ["pao", "maionese", "hambúguer", ]
+let ingredientesLanches = ["Pão, maionese, hambúguer, tomate, alface", "Pão, maionese, hambúguer, tomate, alface, pepino, creamcheese, molho verde", "Pão, carne moída, tomate, alface, brócolis", " Pão, maionese, hambúguer, alface, cebola, azeitona, molho barbecue", "Pão, maionese, hambúguer, tomate, alface, bacon, batata palha", "Pão, maionese, hambúguer, tomate, alface, pepino, molho verde, queijo", "Pão, maionese, hambúguer, tomate, alface, queijo, pepino, bacon, molho verde", "Pão, maionese, carne de rato, tomate, alface", "panqueca, maionese, carne de taco, tomate, alface"];
 
 let cardapio = [nomeLanches, precoLanches, ingredientesLanches];
 
